e40aef65b1a2444e1c064a2a5eec4a20c89264f0
beto <beto@asuvorm.be>
2022-05-29 21:16:01 +0200

    added methods in WordPuzzler to separate target words and partial words and test accepted

--- src/main/java/com/example/sixletterapi/model/WordPuzzler.java
+++ src/main/java/com/example/sixletterapi/model/WordPuzzler.java
@@ -1,4 +1,34 @@
 package com.example.sixletterapi.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class WordPuzzler {
+    private final HashMap<String, HashSet<String>> collection = new HashMap<>();
+    private ArrayList<String> allWords;
+
+    public WordPuzzler(ArrayList<String> allWords){
+        this.allWords = allWords;
+    }
+
+    private HashMap<String, HashSet<String>> getCollection() {
+        return collection;
+    }
+
+    public ArrayList<String> getAllWords() {
+        return allWords;
+    }
+
+    public List<String> getXLetterWords(ArrayList<String> words, int size) {
+        return words.stream().filter(e -> e.length() == size).collect(Collectors.toList());
+    }
+
+    public List<String> getPartialWords(ArrayList<String> words, int size){
+        return words.stream().filter(e -> e.length() == size).collect(Collectors.toList());
+    }
+
+
 }
--- src/test/java/com/example/sixletterapi/SixletterapiApplicationTests.java
+++ src/test/java/com/example/sixletterapi/SixletterapiApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.sixletterapi;
 
 import com.example.sixletterapi.helpers.ImportUtil;
+import com.example.sixletterapi.model.WordPuzzler;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,8 +14,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SixletterapiApplicationTests {
-
     File testInput = new File("src/main/resources/testData/input.txt");
+    WordPuzzler puzzler = new WordPuzzler(ImportUtil.importer(testInput));
+
+    SixletterapiApplicationTests() throws IOException {
+    }
 
     @Test
     void testIfImportUtilWorks() throws IOException {
@@ -33,6 +37,22 @@ class SixletterapiApplicationTests {
         assertTrue(importedWords.containsAll(comparedWords));
     }
 
+    @Test
+    void getXLetterWords() {
+        ArrayList<String> sixLetterWords = (ArrayList<String>) puzzler.getXLetterWords(puzzler.getAllWords(), 6);
+        assertTrue((sixLetterWords.get(1).length() == 6));
+        assertTrue((sixLetterWords.get(5).length() == 6));
+        assertTrue((sixLetterWords.get(10).length() == 6));
+        assertTrue((sixLetterWords.get(sixLetterWords.size()-1).length() == 6));
+    }
 
+    @Test
+    void getPartialWords() {
+        ArrayList<String> partials = (ArrayList<String>) puzzler.getPartialWords(puzzler.getAllWords(), 6);
+        assertTrue((partials.get(1).length() == 6));
+        assertTrue((partials.get(5).length() == 6));
+        assertTrue((partials.get(10).length() == 6));
+        assertTrue((partials.get(partials.size()-1).length() == 6));
+    }
 
 }
