64b51850fffd04147598630a6c969a8b08d2e7e1
beto <beto@asuvorm.be>
2022-05-29 19:15:09 +0200

    project creation, first commit

---
+++ src/main/java/com/example/sixletterapi/SixletterapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sixletterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixletterapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SixletterapiApplication.class, args);
+    }
+
+}
